f0c465bd3b5bfbfd8f836504b365ece8de2faa0b
Jeffrey Way <jeffrey@jeffrey-way.com>
2017-10-13 10:02:40 -0400

    Episode 2

---
+++ episode-2/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span class="count" v-text="count"></span> 
+        <button @click="count++">Increment</button>
+    </div>    
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                count: 0
+            };
+        }
+    }
+</script>
